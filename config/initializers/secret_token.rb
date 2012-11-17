8c5e1332213fd9455d6b51fb45e821a04135bd0b
Chyld Medford <chyld.medford@gmail.com>
2012-11-17 12:07:06 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Songz::Application.config.secret_token = 'f20528b554478a79f47d84ca8318be6082bf3ac88674a3792f3a991e9be8705c67140ed89d9e74358c844d99597b9ff589c83d2d0f46c60a1df9dac34e1984d4'
